8f1093598882eef613b40db79fc9ec5326dee7ba
Jashimuddin123 <adnanmahmud3394@gmail.com>
2025-01-10 18:40:50 +0600

    create a new component

--- src/Components/Expertice.jsx
+++ src/Components/Expertice.jsx
@@ -8,6 +8,50 @@ const Expertice = () => {
             backgroundImage: "url('https://i.ibb.co.com/4dzxKHc/photo-6-1.png')", // Replace with your background image URL
         }} 
         >
+            <div>
+                <h1 className="text-white ">EXPERIENCE SUCCESS</h1>
+                <h1 className="text-white text-5xl font-bold">Experience by <br /> passion & expertise</h1>
+                <p className="text-white text-center w-96 mx-auto">Netus turpis habitasse semper ipsum sollicitudin malesuada. Maecenas posuere potenti litora at varius ante malesuada pulvinar elit</p>
+            </div>
+  
+
+  {/* expertice block */}
+  <div className="flex gap-14 justify-center mt-48">
+  <div className="border border-white hover:bg-green-500 rounded-full w-48 h-48 flex flex-col items-center justify-center">
+    <h1 className="text-3xl text-white">
+      <span className="inline-block hover:animate-bounce">1</span>
+      <span className="inline-block hover:animate-bounce">4</span>
+      <span className="inline-block hover:animate-bounce">+</span>
+    </h1>
+    <p className="text-white">Years Experience</p>
+  </div>
+  <div className="border border-white hover:bg-green-500 rounded-full w-48 h-48 flex flex-col items-center justify-center">
+    <h1 className="text-3xl text-white">
+      <span className="inline-block hover:animate-bounce">1</span>
+      <span className="inline-block hover:animate-bounce">4</span>
+      <span className="inline-block hover:animate-bounce">+</span>
+    </h1>
+    <p className="text-white">Years Experience</p>
+  </div>
+  <div className="border border-white hover:bg-green-500 rounded-full w-48 h-48 flex flex-col items-center justify-center">
+    <h1 className="text-3xl text-white">
+      <span className="inline-block hover:animate-bounce">1</span>
+      <span className="inline-block hover:animate-bounce">4</span>
+      <span className="inline-block hover:animate-bounce">+</span>
+    </h1>
+    <p className="text-white">Years Experience</p>
+  </div>
+  <div className="border border-white hover:bg-green-500 rounded-full w-48 h-48 flex flex-col items-center justify-center">
+    <h1 className="text-3xl text-white">
+      <span className="inline-block hover:animate-bounce">1</span>
+      <span className="inline-block hover:animate-bounce">4</span>
+      <span className="inline-block hover:animate-bounce">+</span>
+    </h1>
+    <p className="text-white">Years Experience</p>
+  </div>
+</div>
+
+
             
         </div>
     );
---
+++ src/Components/GreateIdea.jsx
@@ -0,0 +1,18 @@
+
+
+const GreateIdea = () => {
+    return (
+        <div className="w-full h-[700px] bg-zinc-600">
+            {/*img content */}
+            <div>
+              <img src="" alt="" />
+            </div>
+            {/* text content */}
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default GreateIdea;
